c857c64ca8094c36e557d13dfda027b41e0c8651
maykon <nokiam13@hotmail.com>
2022-09-14 10:09:41 -0300

    arquivo .env incluso o mysql e alteração da porta para 2000, instances mysql.ts, model usando mysql

---
+++ src/instances/mysql.ts
@@ -0,0 +1,24 @@
+import { Sequelize } from 'sequelize'; 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+/* export const sequelize = new Sequelize(
+    process.env.PG_DB as string,
+    process.env.PG_USER as string,
+    process.env.PG_PASSWORD as string,
+    {
+        dialect: 'postgres',
+        port: parseInt(process.env.PG_PORT as string)
+    }
+); */
+
+export const sequelize = new Sequelize(
+    process.env.MYSQL_DB as string,
+    process.env.MYSQL_USER as string,
+    process.env.MYSQL_PASSWORD as string,
+    {
+        dialect: 'mysql',
+        port:  parseInt(process.env.MYSQL_PORT as string)
+    }
+);
